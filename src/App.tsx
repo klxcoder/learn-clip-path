766c6b074cfceef8c778edfff023bdc29d610943
klxcoder <klxcoder@gmail.com>
2025-11-19 21:51:12 +0700

    css center

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className={styles.app}>
-      <div>
+      <div className={styles.exp1}>
         <div className={styles.chooseMode}>
           <label>
             <input
