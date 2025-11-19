0cc897ec11ba85e196361c42aa4950e319b275ca
klxcoder <klxcoder@gmail.com>
2025-11-19 08:37:59 +0700

    scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import './App.css'
+import styles from './App.module.scss'
 import Polygon from './components/Polygon'
 
 function App() {
 
   return (
-    <>
-      <div>Minimal</div>
+    <div className={styles.app}>
       <div>
         <Polygon
           onClick={() => alert('Clicked polygon')}
         />
       </div>
-    </>
+    </div>
   )
 }
 
