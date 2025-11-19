b203a60e4e196b4ff28d4cc08a20015b403df005
klxcoder <klxcoder@gmail.com>
2025-11-19 08:41:40 +0700

    Polygon

--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -1,4 +1,8 @@
 import type { ReactElement } from "react";
+import styles from "./Polygon.module.scss";
+
+const WIDTH: number = 500;
+const HEIGHT: number = 500;
 
 interface PolygonProps {
   readonly onClick: () => void;
@@ -7,16 +11,24 @@ interface PolygonProps {
 function Polygon({ onClick }: PolygonProps): ReactElement {
   return (
     <div
-      onClick={onClick}
+      className={styles.app}
       style={{
-        width: '200px',
-        height: '200px',
-        backgroundColor: 'rgba(255,0,0,0.3)',
-        border: '10px solid blue',
-        cursor: 'pointer',
-        clipPath: 'polygon(0% 0%, 100% 0%, 80% 100%, 20% 100%)',
+        width: WIDTH,
+        height: HEIGHT,
       }}
-    />
+    >
+      <div
+        onClick={onClick}
+        style={{
+          width: '200px',
+          height: '200px',
+          backgroundColor: 'rgba(255,0,0,0.3)',
+          border: '10px solid blue',
+          cursor: 'pointer',
+          clipPath: 'polygon(0% 0%, 100% 0%, 80% 100%, 20% 100%)',
+        }}
+      />
+    </div>
   );
 }
 
