f7af8dbf60c6a99ab088265179fe187dbfcda549
klxcoder <klxcoder@gmail.com>
2025-11-19 10:37:48 +0700

    detect Left and Right click

--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -4,6 +4,12 @@ import styles from "./Polygon.module.scss";
 const WIDTH: number = 500;
 const HEIGHT: number = 500;
 
+const MouseButton = {
+  Left: 0,
+  Middle: 1,
+  Right: 2,
+} as const;
+
 function Polygon(): ReactElement {
   const divRef = useRef<HTMLDivElement>(null);
   return (
@@ -21,7 +27,12 @@ function Polygon(): ReactElement {
         const offsetY: number = e.clientY - rect.top;
         const percentX: number = offsetX / WIDTH * 100;
         const percentY: number = offsetY / HEIGHT * 100;
-        console.log({ mouseButton: e.button, percentX, percentY })
+        const mouseButton: number = e.button;
+        if (mouseButton === MouseButton.Left) {
+          console.log('Left', { percentX, percentY });
+        } else if (mouseButton === MouseButton.Right) {
+          console.log('Right', { percentX, percentY });
+        }
       }}
       onContextMenu={(e) => e.preventDefault()}
     >
